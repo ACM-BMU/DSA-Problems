86f6ca22fdb9feb9c9f156fbfc4e57c2c9ec4088
07Atharva <72624739+07Atharva@users.noreply.github.com>
2021-10-23 14:48:31 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,29 +1,24 @@
 #include <iostream>
 using namespace std;
 
-void towers(int, char, char, char);
-
-void towers(int num, char mainpeg, char topeg, char auxpeg)
-{
-    if (num == 1)
-    {
-        cout << " Move disk 1 from peg " << mainpeg << " to peg " << topeg << endl;
-        return;
-    }
-    towers(num - 1, mainpeg, auxpeg, topeg);
-    cout << " Move disk " << num << " from peg " << mainpeg << " to peg " << topeg << endl;
-    towers(num - 1, auxpeg, topeg, mainpeg);   
-} 
+void tim(int, int);
 
 int main()
 {
-    int num;
-    cout << "Enter number of disks : ";
-    cin >> num;
-    cout << "The sequence of moves involved at Tower of Hanoi :\n" << endl;
-    towers(num, 'A', 'B', 'C');
-    
-    cin.get();
-    cin.get();
-    return 0;
+	int hour, min;
+
+	cout << "Enter the number of hours: " << endl;
+	cin >> hour;
+	cout << "Enter the number of minutes: " << endl;
+	cin >> min;
+	
+	tim(hour, min);
+
+	cin.get();
+	return 0;
+}
+void tim(int h, int m)
+{
+	cout<<"Time: "<<h<<":"<<m<<endl;
+
 }
